51adc7aa8269e98253b4f8f142a2cce942e94aa9
sureshbanoth-robosoft <suresh.banoth@robosoftin.com>
2026-09-23 19:34:48 +0530

    Update infra errors

--- aws-static-site/infra/terraform/main.tf
+++ aws-static-site/infra/terraform/main.tf
@@ -31,17 +31,24 @@ resource "aws_s3_bucket_ownership_controls" "site" {
   bucket = aws_s3_bucket.site.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
+}
+
+resource "aws_s3_bucket_acl" "site" {
+  depends_on = [aws_s3_bucket_ownership_controls.site]
+
+  bucket = aws_s3_bucket.site.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_cloudfront_origin_access_control" "site" {
