87a358f46b664f9ee2d4383699d7168f4395a091
sureshbanoth-robosoft <suresh.banoth@robosoftin.com>
2026-09-23 20:20:33 +0530

    Remove Route53 from Terraform

--- aws-static-site/infra/terraform/main.tf
+++ aws-static-site/infra/terraform/main.tf
@@ -126,34 +126,6 @@ resource "aws_cloudfront_distribution" "site" {
   tags = local.tags
 }
 
-resource "aws_route53_zone" "primary" {
-  name = var.domain_name
-}
-
-resource "aws_route53_record" "apex" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.site.domain_name
-    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "apex_aaaa" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.domain_name
-  type    = "AAAA"
-
-  alias {
-    name                   = aws_cloudfront_distribution.site.domain_name
-    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
 output "bucket_name" {
   value = aws_s3_bucket.site.bucket
 }
